30d5d46817f518f92bedef608e3116df2c32cf59
Justin Kim <justinkim2015@gmail.com>
2023-02-11 09:17:36 +0900

    Finish basic routing via url

--- index.js
+++ index.js
@@ -5,19 +5,34 @@ const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 8080;
 
-const index = fs.readFile("index.html", (err, data) => {
-  if (err) throw err;
-  return data
-});
-
-console.log(index)
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/contact") {
+    fs.readFile("contact.html", (err, data) => {
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile("404.html", (err, data) => {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
